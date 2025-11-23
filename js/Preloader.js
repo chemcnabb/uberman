c97878f1044bcfaf1e35038533fb2f12c16ca3ac
chemcnabb <chemcnabb@gmail.com>
2025-11-23 00:01:57 -0500

    Update dependencies and streamline preload assets

--- js/Preloader.js
+++ js/Preloader.js
@@ -6,15 +6,11 @@ Uberman.Preloader = function (Uberman) {
 };
 
 Uberman.Preloader.prototype = {
-
-
   preload: function () {
-
     this.game.load.bitmapFont('digits', 'font/digits.png', 'font/digits.xml');
     this.game.load.bitmapFont('font', 'font/font.png', 'font/font.xml');
     this.game.load.bitmapFont('smallfont', 'font/small_font.png', 'font/small_font.xml');
 
-    //this.game.load.image('platform', 'sprites/platform.png');
     this.game.load.image('door', 'images/door.gif', 70, 80);
     this.game.load.image('orbit', 'images/orbit.png', 4267, 894);
     this.game.load.image('ground', 'images/ground.gif', 4267, 10);
@@ -22,34 +18,18 @@ Uberman.Preloader.prototype = {
     this.game.load.image('city_background', 'images/city_background.gif', 4267, 2133);
     this.game.load.image('city_fade', 'images/city_fade_background.gif', 4267, 2133);
 
-    // BUILDING 1
-    this.game.load.image('floor_1', 'images/buildings/building1_floor.gif', 428, 25);
-    this.game.load.image('ground_1', 'images/buildings/building1_ground.gif', 428, 98);
-    this.game.load.image('top_1', 'images/buildings/building1_top.gif', 428, 34);
-    // BUILDING 2
-    this.game.load.image('floor_2', 'images/buildings/building2_floor.gif', 273, 52);
-    this.game.load.image('ground_2', 'images/buildings/building2_ground.gif', 273, 41);
-    this.game.load.image('top_2', 'images/buildings/building2_top.gif', 273, 78);
-    // BUILDING 3
-    this.game.load.image('floor_3', 'images/buildings/building3_floor.gif', 301, 136);
-    this.game.load.image('ground_3', 'images/buildings/building3_ground.gif', 301, 50);
-    this.game.load.image('top_3', 'images/buildings/building3_top.gif', 301, 111);
-// BUILDING 4
-    this.game.load.image('floor_4', 'images/buildings/building4_floor.gif', 501, 52);
-    this.game.load.image('ground_4', 'images/buildings/building4_ground.gif', 501, 85);
-    this.game.load.image('top_4', 'images/buildings/building4_top.gif', 501, 166);
+    this.loadBuildingSet([
+      {id: 1, floor: {w: 428, h: 25}, ground: {w: 428, h: 98}, top: {w: 428, h: 34}},
+      {id: 2, floor: {w: 273, h: 52}, ground: {w: 273, h: 41}, top: {w: 273, h: 78}},
+      {id: 3, floor: {w: 301, h: 136}, ground: {w: 301, h: 50}, top: {w: 301, h: 111}},
+      {id: 4, floor: {w: 501, h: 52}, ground: {w: 501, h: 85}, top: {w: 501, h: 166}}
+    ]);
 
     this.game.load.image('bank', 'images/buildings/bank.gif', 288, 279);
     this.game.load.image('library', 'images/buildings/Library.gif', 596, 231);
-
-
-
-
-
     this.game.load.image('bakery', 'images/buildings/bakery.gif');
     this.game.load.image('cafe', 'images/buildings/cafe.gif');
     this.game.load.image('bookstore', 'images/buildings/bookstore.gif');
-
 
     this.game.load.image('cape_streak', 'images/cape_streak.png');
     this.game.load.image('sun', 'images/sun.png');
@@ -60,7 +40,6 @@ Uberman.Preloader.prototype = {
     this.game.load.image('car2', 'images/car2.gif', 270, 81);
     this.game.load.image('uber_disk', 'images/UBER_DISK.png', 138,138);
     this.game.load.image('alter_disk', 'images/ALTER_DISK.png', 138,138);
-
   },
 
 
@@ -80,6 +59,17 @@ Uberman.Preloader.prototype = {
 
   render: function () {
 
+  },
+
+  loadBuildingSet: function (buildingParts) {
+    for (var i = 0; i < buildingParts.length; i++) {
+      var part = buildingParts[i];
+      var basePath = 'images/buildings/';
+
+      this.game.load.image('floor_' + part.id, basePath + 'building' + part.id + '_floor.gif', part.floor.w, part.floor.h);
+      this.game.load.image('ground_' + part.id, basePath + 'building' + part.id + '_ground.gif', part.ground.w, part.ground.h);
+      this.game.load.image('top_' + part.id, basePath + 'building' + part.id + '_top.gif', part.top.w, part.top.h);
+    }
   }
 
 };
